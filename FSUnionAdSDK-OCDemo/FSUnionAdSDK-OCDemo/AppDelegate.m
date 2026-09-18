41a2571a6bae4e8e91769b3c16da8e0539ae2310
yoser <lihs@zenmen.com>
2026-09-18 19:28:20 +0800

    feat(版本更新)[1.0.15.1]：若干已知问题修复

--- FSUnionAdSDK-OCDemo/FSUnionAdSDK-OCDemo/AppDelegate.m
+++ FSUnionAdSDK-OCDemo/FSUnionAdSDK-OCDemo/AppDelegate.m
@@ -30,6 +30,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     // initialize AD SDK
+    
     [self setupFSAdSDK];
     
     // fetch IDFA
